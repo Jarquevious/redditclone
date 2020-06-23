a010c2a5c282920edf688b7e05e0fe80e3d4ad92
Jarquevious <jarquevious@gmail.com>
2020-06-22 19:36:58 -0700

    new post complete

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 // Initialize express
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 // require handlebars
 const exphbs = require('express-handlebars');
-
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
-
 // Render the "home" layout for the main page and send the following msg
-app.get('/', (req, res) => {
-    res.render('home', {});
-  })
 
+app.get('/', (req, res) => {
+    res.render('home', {msg:Reddit});
+  })
+  
+app.get('/posts/new', (req,res) =>{
+    res.render('posts-new.handlebars');
+})
+
+// app.post('/posts/new', (req, res) => {
+//     res.render('post-new');
+//   });
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
 
