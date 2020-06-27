c89138e249aa1571265d0cc09462151cf0213632
Jarquevious <jarquevious@gmail.com>
2020-06-26 18:14:01 -0700

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 // Initialize express
 const express = require('express');
 const app = express();
-// require handlebars
+// get data back from db 
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+});
 // Use "main" as our default layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', hbs.engine);
 // Use handlebars to render
 app.set('view engine', 'handlebars');
+
+
+
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+require('./data/reddit-db');
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
 // Render the "home" layout for the main page and send the following msg
 
-app.get('/', (req, res) => {
-    res.render('home', {msg:Reddit});
-  })
-  
-app.get('/posts/new', (req,res) =>{
-    res.render('posts-new.handlebars');
-})
-
+require('./controllers/posts.js')(app);
 // app.post('/posts/new', (req, res) => {
 //     res.render('post-new');
 //   });
